a5fd6e225d8d8b87ee63b2b198f98bf86b9f58a2
Nikhitha <162926810+fallenscent22@users.noreply.github.com>
2025-01-11 13:27:11 +0530

    Update index.js
    Signed-off-by: Nikhitha  <162926810+fallenscent22@users.noreply.github.com>

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://nikhithachatla6:nikhitha@digitalwallet.hbil4.mongodb.net/test', {
+mongoose.connect('mongodb url', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
